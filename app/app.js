7bd29c34daebb14621093d404b785ad44c05fc63
strepito66 <davidecarraro428@gmail.com>
2024-02-03 22:45:00 +0100

    Fixed age calculation.

--- app/app.js
+++ app/app.js
@@ -23,12 +23,17 @@ $(document).ready(function () {
     });
 
     function renderAgeLoop() {
+        var minutes = 1000 * 60;
+        var hours = minutes * 60;
+        var days = hours * 24;
+        var years1 = days * 365.25;
         localStorageData = localStorage.birthdayDate;
         setInterval(function () {
             parsingData = new Date(parseInt(localStorageData));
             let now = new Date;
             let duration = now - parsingData;
-            let years = duration / 31556900000; // 1 year in millisecond
+
+            let years = duration / years1;
 
             let majorMinor = years.toFixed(9).toString().split('.');
             $('#year').text(majorMinor[0]);
